ca3f701bb1a00311c9bad3148cf2118d43740369
Imran Melikov <imran.melikov70@gmail.com>
2023-04-04 23:41:37 +0400

    commit

--- app/src/main/java/com/imranmelikov/easyfood/Activities/MealActivity.kt
+++ app/src/main/java/com/imranmelikov/easyfood/Activities/MealActivity.kt
@@ -5,15 +5,19 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
+import com.imranmelikov.easyfood.Db.MealDatabase
 import com.imranmelikov.easyfood.Fragments.HomeFragment
 import com.imranmelikov.easyfood.R
 import com.imranmelikov.easyfood.databinding.ActivityMealBinding
 import com.imranmelikov.easyfood.pojo.Meal
 import com.imranmelikov.easyfood.viewModel.MealViewModel
+import com.imranmelikov.easyfood.viewModel.MealViewModelFactory
 
 class MealActivity : AppCompatActivity() {
     private lateinit var mealId:String
@@ -28,7 +32,10 @@ class MealActivity : AppCompatActivity() {
         val view=binding.root
         setContentView(view)
 
-        mealMvvm=ViewModelProviders.of(this)[MealViewModel::class.java]
+        val mealDatabase=MealDatabase.getInstance(this)
+        val viewModelFactory=MealViewModelFactory(mealDatabase)
+        mealMvvm=ViewModelProvider(this,viewModelFactory).get(MealViewModel::class.java)
+
         getMealInformationfromintent()
         setInformationInView()
         loadimagecase()
@@ -37,6 +44,17 @@ class MealActivity : AppCompatActivity() {
         observerMealDetailsLiveData()
 
         onYoutubeimageclick()
+
+        onFavoriteclick()
+    }
+
+    private fun onFavoriteclick() {
+        binding.btnSave.setOnClickListener {
+              mealToSave?.let {
+                  mealMvvm.innerMeal(it)
+                  Toast.makeText(this,"meal save",Toast.LENGTH_SHORT).show()
+              }
+        }
     }
 
     private fun onYoutubeimageclick() {
@@ -47,11 +65,14 @@ class MealActivity : AppCompatActivity() {
 
     }
 
+   private var mealToSave:Meal?=null
     private fun observerMealDetailsLiveData() {
         mealMvvm.observerMealDetailsLiveData().observe(this,object :Observer<Meal>{
             override fun onChanged(value: Meal) {
                 onResponseCase()
                 val meal=value
+                mealToSave=meal
+
                 binding.tvCategoryInfo.text=meal.strCategory
                 binding.tvAreaInfo.text=meal.strArea
                 binding.tvInstructions.text=meal.strInstructions
--- app/src/main/java/com/imranmelikov/easyfood/viewModel/MealViewModel.kt
+++ app/src/main/java/com/imranmelikov/easyfood/viewModel/MealViewModel.kt
@@ -4,15 +4,21 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.imranmelikov.easyfood.Activities.MainActivity
+import com.imranmelikov.easyfood.Db.MealDatabase
 import com.imranmelikov.easyfood.pojo.Meal
 import com.imranmelikov.easyfood.pojo.Meallist
 import com.imranmelikov.easyfood.retrofit.RetrofitInstance
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
 
-class MealViewModel:ViewModel() {
+class MealViewModel(
+    val mealDatabase: MealDatabase
+) :ViewModel(
+) {
     private var mealdetailsLiveData=MutableLiveData<Meal>()
 
     fun getMealDetail(id:String){
@@ -34,5 +40,16 @@ class MealViewModel:ViewModel() {
     }
     fun observerMealDetailsLiveData():LiveData<Meal>{
         return mealdetailsLiveData
+    }
+
+    fun innerMeal(meal:Meal){
+        viewModelScope.launch {
+            mealDatabase.mealDao().upsert(meal)
+        }
+    }
+    fun deleteMeal(meal:Meal){
+        viewModelScope.launch {
+            mealDatabase.mealDao().delete(meal)
+        }
     }
 }
